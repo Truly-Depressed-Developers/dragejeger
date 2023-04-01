4bf881617ff67758d72299e496b7960480d0c130
Krzysztof Wrona <rubikon02@gmail.com>
2023-04-01 21:11:28 +0200

    Dino run better

--- Assets/Scripts/DinoRun/Player.cs
+++ Assets/Scripts/DinoRun/Player.cs
@@ -8,9 +8,10 @@ public class Player : MonoBehaviour
    [SerializeField] private Rigidbody2D rigidbody_;
    [SerializeField] private Camera camera_;
 
-   [SerializeField] private float force_ = 8f;
+   [SerializeField] private float jump_speed_ = 20f;
 
    private float _playerWidth;
+   private bool sneaking => Input.GetKey("s");
    private float _playerHeight;
 
    bool isJumping_ = false;
@@ -36,11 +37,17 @@ public class Player : MonoBehaviour
    {
       if (Input.GetKey("w") && !isJumping_)
       {
-         rigidbody_.velocity = new Vector2(0, force_);
+         rigidbody_.velocity = new Vector2(0, jump_speed_);
 
          // rigidbody_.AddForce(new Vector2(0, force_));
 
          isJumping_ = true;
+      }
+      if (sneaking)
+      {
+         transform.localScale = new Vector3(1, 0.5f , 1);
+      } else {
+         transform.localScale = new Vector3(1, 1, 1);
       }
 
       if (rigidbody_.velocity.y == 0)
@@ -73,28 +80,32 @@ public class Player : MonoBehaviour
    //    }
    // }
 
+   private void OnTriggerEnter2D(Collider2D other) {
+        SetIsAliveToNot();
+   }
+
    void CheckIfHitsWall()
    {
-      float heightRange = (_playerHeight / 2) * 0.6f;
+    //   float heightRange = (_playerHeight / 2) * 0.6f;
 
-      Vector2 hitVector2_1 = new Vector2(transform.position.x + _playerWidth / 2, transform.position.y - heightRange);
-      Vector2 hitVector2_2 = new Vector2(transform.position.x + _playerWidth / 2, transform.position.y + heightRange);
-      RaycastHit2D hit1 = Physics2D.Raycast(hitVector2_1, Vector2.right, 0.1f);
-      RaycastHit2D hit2 = Physics2D.Raycast(hitVector2_2, Vector2.right, 0.1f);
+    //   Vector2 hitVector2_1 = new Vector2(transform.position.x + _playerWidth / 2, transform.position.y - heightRange);
+    //   Vector2 hitVector2_2 = new Vector2(transform.position.x + _playerWidth / 2, transform.position.y + heightRange);
+    //   RaycastHit2D hit1 = Physics2D.Raycast(hitVector2_1, Vector2.right, 0.1f);
+    //   RaycastHit2D hit2 = Physics2D.Raycast(hitVector2_2, Vector2.right, 0.1f);
 
-      if (hit1)
-      {
-         Debug.Log(hit1.transform.name);
-      }
+    //   if (hit1)
+    //   {
+    //      Debug.Log(hit1.transform.name);
+    //   }
 
-      if (hit2)
-      {
-         Debug.Log(hit2.transform.name);
-      }
-      if (hit1 || hit2)
-      {
-         SetIsAliveToNot();
-      }
+    //   if (hit2)
+    //   {
+    //      Debug.Log(hit2.transform.name);
+    //   }
+    //   if (hit1 || hit2)
+    //   {
+    //      SetIsAliveToNot();
+    //   }
    }
 
    void SetIsAliveToNot()

9c270c81aeea143967427554b0efb00dd0f19ff1
Krzysztof Wrona <rubikon02@gmail.com>
2023-04-02 04:49:41 +0200

    yyy

---
+++ Assets/Scripts/DinoRun/MainCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+   public float cameraHeight; // half of real height
+   public float aspectRatio;
+   public float cameraWidth; // half of real width
+
+   public float xPosition;
+   public float yPosition;
+
+   public static MainCamera instance;
+
+   // Start is called before the first frame update
+   void Awake()
+   {
+      cameraHeight = Camera.main.orthographicSize;
+      aspectRatio = Screen.width / (float)Screen.height;
+      cameraWidth = cameraHeight * aspectRatio;
+
+      xPosition = Camera.main.transform.position.x;
+      yPosition = Camera.main.transform.position.y;
+
+      instance = this;
+   }
+
+   private void Update()
+   {
+      xPosition = Camera.main.transform.position.x;
+      yPosition = Camera.main.transform.position.y;
+   }
+}
